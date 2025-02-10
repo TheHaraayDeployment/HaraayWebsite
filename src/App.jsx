03ed6a52f928ef7b5a47daee0c59d480c6938dfb
HaraayStudio <haraaydeployment@gmail.com>
2025-02-10 11:54:25 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import Contact from "./pages/Contact.jsx";
 import Expertise from "./pages/Expertise.jsx";
 import Blogs from "./pages/Blogs.jsx";
 import Blog01 from "./pages/blogs/Blog1.jsx";
-import Blog02 from "./pages/blogs/Blog2.jsx";
+import Blog2 from "./pages/blogs/Blog2.jsx";
 import Service1 from "./pages/Service1.jsx";
 import Service2 from "./pages/Service2.jsx";
 import Service3 from "./pages/Service3.jsx";
@@ -153,7 +153,7 @@ function App() {
           <Route path="/about" element={<AboutUs />} />/
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blogs/blog01" element={<Blog01 />} />
-          <Route path="/blogs/blog02" element={<Blog02 />} />
+          <Route path="/blogs/blog02" element={<Blog2 />} />
           <Route path="/contact-us" element={<Contact />} />
           <Route path="/faq" element={<FaqPage />} />
           <Route path="/expertise" element={<Expertise />} />
