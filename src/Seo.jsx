8a07448c8b698c09d6661ca9c81be816193ebc98
HaraayStudio <189243006+HaraayStudio@users.noreply.github.com>
2025-04-10 10:17:13 +0530

    added

--- src/Seo.jsx
+++ src/Seo.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
-
 const SEO = ({ title, description, keywords }) => {
   return (
     <Helmet>
-      <title>{title} | Satvik Raas</title>
+      <title>{title} | Haraay</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
     </Helmet>
   );
 };
-
 export default SEO;
